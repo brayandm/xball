3853dfda63265d0c956c725e0ca2f1b9f3d4d978
Brayan Durán Medina <brayan.duran@leaguesofcode.com>
2023-07-25 12:30:58 +0200

    Player component added

---
+++ src/components/Player/Player.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+import styles from './Player.module.css';
+
+export default function Player() {
+    return (
+        <div className={styles.player}>
+            </div>
+    );
+}
