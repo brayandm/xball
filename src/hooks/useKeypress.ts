83dfb5e367f80eced303b8290390854b6567f187
Brayan Durán Medina <brayan.duran@leaguesofcode.com>
2023-07-25 12:33:12 +0200

    pretty

--- src/hooks/useKeypress.ts
+++ src/hooks/useKeypress.ts
@@ -1,13 +1,13 @@
-import { useEffect } from 'react';
+import { useEffect } from "react";
 
 const useKeypress = (handler: (key: string) => void) => {
   useEffect(() => {
     const eventListener = (event: KeyboardEvent) => {
       handler(event.key);
     };
-    window.addEventListener('keydown', eventListener);
+    window.addEventListener("keydown", eventListener);
     return () => {
-      window.removeEventListener('keydown', eventListener);
+      window.removeEventListener("keydown", eventListener);
     };
   }, [handler]);
 };
